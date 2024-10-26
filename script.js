3adaf716348bbe1b8782cc5c780ccd0dba8dbe75
Khushi Rai <155308071+Khushicoder04@users.noreply.github.com>
2024-10-26 12:42:21 +0530

    Add files via upload
    This project I created by using Html, CSS and JavaScript. This is a to do list app in this we can add a thing that what we have to do and further we can delete if we had done that works. So this is the work of this project.

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    
+    if (taskInput.value.trim() === "") {
+      alert("Please enter a task!");
+      return;
+    }
+  
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${taskInput.value}
+      <button onclick="deleteTask(this)">Delete</button>
+    `;
+  
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+  
+  function deleteTask(button) {
+    const li = button.parentElement;
+    li.remove();
+  }
+  
